cfc931474c644e18f5a12e55ec17e3d30dbd5145
guilherme Silva Schultz <guilhermeSschultz.contato@gmail.com>
2021-10-17 20:43:20 -0300

    add modulo de comandos

--- db_init.py
+++
@@ -1,8 +0,0 @@
-from database.database import *
-from models import luminosidade, temperatura
-
-def init_db():
-    db_base.metadata.create_all(bind=engine)
-
-if __name__ == "__main__":
-    init_db()
