630ae7b7909b2a3c68a2391f51ccdc93d14fada7
guilherme Silva Schultz <guilhermeSschultz.contato@gmail.com>
2021-10-17 19:17:51 -0300

    add  SerializerMixin

--- models/luminosidade.py
+++ models/luminosidade.py
@@ -1,6 +1,7 @@
 from extension.database import db
+from sqlalchemy_serializer import SerializerMixin
 
-class Luminosidade (db_base):
+class Luminosidade (db.Model, SerializerMixin):
     __tablename__ = "luminosidade"
     id   = db.Column(db.Integer, primary_key = True)
     data = db.Column(db.Date)
