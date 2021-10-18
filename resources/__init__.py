a3720cc0ad371ab90ca1327f248e7b8a8384d2b7
guilherme Silva Schultz <guilhermeSschultz.contato@gmail.com>
2021-10-17 21:04:30 -0300

    bug fix

--- resources/__init__.py
+++ resources/__init__.py
@@ -8,7 +8,7 @@ bp = Blueprint("my_api", __name__, url_prefix='/api_V1')
 api = Api(bp)
 api.add_resource(Temp_resource, '/temp/<data>')
 api.add_resource(Temp_resource_get_all,'/temp/get_all/')
-api.add_resource(lux_resource, '/lux/<data>')
+api.add_resource(Lux_resource, '/lux/<data>')
 api.add_resource(Lux_resource_get_all, '/lux/get_all/')
 
 def init_app(app):
